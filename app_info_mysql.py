1c27139ecc9b1b1ea7a6f49d0c8fd0fed43bc634
dayiguizhen <709252908@qq.com>
2016-04-05 19:05:53 +0800

    fix: script: 1.add update func.2.return more app info

--- app_info_mysql.py
+++ app_info_mysql.py
@@ -49,6 +49,7 @@ def get_package_info(packagename):
         app_description = data.find('div',{'jsname': 'C4s9Ed'}).get_text();
         app_description = app_description.encode('utf-8');
         app_datepublish = data.find('div',{'itemprop': 'datePublished'}).string;
+        app_icon_url = app_icon = data.find('img',{'class': 'cover-image'}).get('src');
         if data.find('div',{'itemprop': 'softwareVersion'}) is not None:
             app_softversion = data.find('div',{'itemprop': 'softwareVersion'}).string;
         else:
@@ -68,6 +69,7 @@ def get_package_info(packagename):
         for app_url in meta_app_screenshot:
             url = app_url.get("src");
             app_screenshot.append(url);
+        app_screenshot = str(app_screenshot);
         
         app_data = {
             'id': None,
@@ -82,6 +84,10 @@ def get_package_info(packagename):
             'fileSize': app_filesize,
             'operatingSystems': app_operatingSystems,
             'numDownloads': app_downloads,
+            'app_icon_url': app_icon_url,
+            'app_screenshot': app_screenshot,
+            'Google_Play_Link': package_link,
+            'packagename': packagename,
             }
         
         return app_data;
@@ -91,17 +97,37 @@ cursor = conn.cursor();
 
 def mysql_insert(packagename):
     myDict = get_package_info(packagename);
-    insert_table = 'app_info';
     placeholders = ','.join(['%s']* len(myDict));
     colums = ','.join(myDict.keys());
-    insert_sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (insert_table,colums,placeholders);
+    insert_sql = "INSERT INTO app_info ( %s ) VALUES ( %s )" % (colums,placeholders);
     cursor.execute(insert_sql,myDict.values());
     conn.commit();
+
+def mysql_select(packagename):
+    select_sql = "SELECT * FROM app_info WHERE packagename = '%s'" %(packagename);
+    flag = cursor.execute(select_sql);
+    results = cursor.fetchall();
+    return flag;
+
+
+def mysql_update(packagename):
+    myDict = get_package_info(packagename);
+    myDict.pop("id",None);
+    placeholders = ','.join(['%s']* len(myDict));
+    colums = ','.join(myDict.keys());
+    update_sql = "UPDATE app_info SET category = %s, name = %s, datepublish = %s, app_icon_url = %s, Google_Play_Link = %s, description = %s, softwareVersion = %s, operatingSystems = %s, rate = %s,score = %s, fileSize = %s, app_screenshot = %s, numDownloads = %s, developer = %s WHERE packagename = %s";
+    cursor.execute(update_sql,myDict.values());
+    conn.commit();
+
+
 
 f = open('packagename.txt').read();
 package_list = f.split('\r\n');
 
 if __name__ == '__main__':
     for packagename in package_list:
-        mysql_insert(packagename);
+        if mysql_select(packagename):
+            mysql_update(packagename);
+        else:
+            mysql_insert(packagename);
         time.sleep(3);
