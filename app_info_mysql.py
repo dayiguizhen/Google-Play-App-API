13d406c6dd7e2e9703fc1f9812b21cd0379d4d6e
dayiguizhen <709252908@qq.com>
2016-07-17 14:56:59 +0800

    fix:bugs:1.add app price to category info.

--- app_info_mysql.py
+++ app_info_mysql.py
@@ -3,117 +3,117 @@ import urllib2,time
 import MySQLdb
 import httplib
 import urlparse
-from bs4 import BeautifulSoup
 import datetime
+from bs4 import BeautifulSoup
 
 today = str(datetime.date.today());
 database = today + '_google_app';
 database = database.replace('-','_');
-
-
 cookie_str = 'you cookie str';
 
-def request(url,cookie=''):
-    ret = urlparse.urlparse(url);
-    if ret.scheme == 'http':
-        conn = httplib.HTTPConnection(ret.netloc);
-    elif ret.scheme == 'https':
-        conn = httplib.HTTPSConnection(ret.netloc);
-
-    url = ret.path;
-    if ret.query: url += '?' + ret.query;
-    if ret.fragment: url += '#' + ret.fragment;
-    if not url: url = '/';
-
-    conn.request(method="GET",url=url,headers={'Cookie': cookie});
-    return conn.getresponse();
-
 def get_package_info(packagename):
+    print "[" + datetime.datetime.now().strftime('%b-%d-%y %H:%M:%S') + "] " + packagename;
     link = 'https://play.google.com/store/apps/details?id=';
     package_link = link + packagename;
-    web = request(package_link,cookie_str);
-    if web.status == 404:
-        print "CR";
-        app_data = {};
-        return app_data;
-    if web.status == 403:
-        print "403";
-        app_data = {};
-        return app_data;
-    else:
-        web = web.read();
-        #data = BeautifulSoup(web,'html5lib');
-        data = BeautifulSoup(web,"html.parser");
-        print packagename;
-        
-        app_name = data.find('div',{'class': 'id-app-title'}).string;
-        app_name = app_name.encode('utf-8');
+    request = urllib2.Request(package_link,headers={'Accept-Language': 'en_US','Cookie':cookie_str});
+    try:
+        web = urllib2.urlopen(request);
+    except urllib2.HTTPError as e:
+        if e.code == 404:
+            print packagename + ' is 404';
+            app_data = {};
+            return app_data;
+        if e.code == 403:
+            print packagename + "403";
+            app_data = {};
+            return app_data;
+    web = web.read();
+    data = BeautifulSoup(web,"html.parser");
+    app_name = data.find('div',{'class': 'id-app-title'}).string;
+    app_name = app_name.encode('utf-8');
+    if data.find('span',{'itemprop': 'name'}) is not None:
         app_developer = data.find('span',{'itemprop': 'name'}).string;
         app_developer = app_developer.encode('utf-8');
-        app_category = data.find('span',{'itemprop': 'genre'}).string;
-        app_category = app_category.encode('utf-8')
-        #获取App的应用评价数
-        if data.find('span',{'class':'reviewers-small'}) is not None:
-            app_rate = data.find('span',{'class':'reviewers-small'}).get('aria-label');
-        else:
-            app_rate = 0;
-        #获取App的应用评分
-        if data.find('div',{'class': 'score'}) is not None:
-            app_score = data.find('div',{'class': 'score'}).string;
-        else:
-            app_score = None;
-        app_description = data.find('div',{'jsname': 'C4s9Ed'}).get_text();
-        app_description = app_description.encode('utf-8');
+    else:
+        app_developer = None;
+    app_category = data.find('span',{'itemprop': 'genre'}).string;
+    app_category = app_category.encode('utf-8');
+    #获取App的应用评价数
+    if data.find('span',{'class':'reviewers-small'}) is not None:
+        app_rate = data.find('span',{'class':'reviewers-small'}).get('aria-label');
+    else:
+        app_rate = 0;
+    #获取App的应用评分
+    if data.find('div',{'class': 'score'}) is not None:
+        app_score = data.find('div',{'class': 'score'}).string;
+    else:
+        app_score = None;
+    app_description = data.find('div',{'jsname': 'C4s9Ed'}).get_text();
+    app_description = app_description.encode('utf-8');
+    if data.find('div',{'itemprop': 'datePublished'}) is not None:
         app_datepublish = data.find('div',{'itemprop': 'datePublished'}).string;
-        app_icon_url = app_icon = data.find('img',{'class': 'cover-image'}).get('src');
-        if data.find('div',{'itemprop': 'softwareVersion'}) is not None:
-            app_softversion = data.find('div',{'itemprop': 'softwareVersion'}).string;
-        else:
-            app_softversion = None;
-        if data.find('div',{'itemprop': 'fileSize'}) is not None:
-            app_filesize = data.find('div',{'itemprop': 'fileSize'}).string;
-        else:
-            app_filesize = None;
+        app_datepublish = app_datepublish.encode('utf-8');
+    else:
+        app_datepublish = None;
+    app_icon_url = app_icon = data.find('img',{'class': 'cover-image'}).get('src');
+    if data.find('div',{'itemprop': 'softwareVersion'}) is not None:
+        app_softversion = data.find('div',{'itemprop': 'softwareVersion'}).string;
+        app_softversion = app_softversion.encode('utf-8');
+    else:
+        app_softversion = None;
+    if data.find('div',{'itemprop': 'fileSize'}) is not None:
+        app_filesize = data.find('div',{'itemprop': 'fileSize'}).string;
+    else:
+        app_filesize = None;
+    if data.find('div',{'itemprop': 'operatingSystems'}) is not None:
         app_operatingSystems = data.find('div',{'itemprop': 'operatingSystems'}).string;
-        if data.find('div',{'itemprop': 'numDownloads'}) is not None:
-            app_downloads = data.find('div',{'itemprop': 'numDownloads'}).string;
-        else:
-            app_downloads = 0;
-        
-        meta_app_screenshot = data.find_all('img',{'itemprop': 'screenshot'}); #screenshot image url list
-        app_screenshot = [];
-        for app_url in meta_app_screenshot:
-            url = app_url.get("src");
-            app_screenshot.append(url);
-        app_screenshot = str(app_screenshot);
-        
-        app_data = {
-            'id': None,
-            'name': app_name,
-            'developer': app_developer,
-            'category': app_category,
-            'rate': app_rate,
-            'score': app_score,
-            'description': app_description,
-            'datepublish': app_datepublish,
-            'softwareVersion': app_softversion,
-            'fileSize': app_filesize,
-            'operatingSystems': app_operatingSystems,
-            'numDownloads': app_downloads,
-            'app_icon_url': app_icon_url,
-            'app_screenshot': app_screenshot,
-            'Google_Play_Link': package_link,
-            'packagename': packagename,
-            }
-        
-        #print app_data;
-        return app_data;
+    else:
+        app_operatingSystems = None;
+    if data.find('div',{'itemprop': 'numDownloads'}) is not None:
+        app_downloads = data.find('div',{'itemprop': 'numDownloads'}).string;
+    else:
+        app_downloads = 0;
+    
+    meta_app_screenshot = data.find_all('img',{'itemprop': 'screenshot'}); #screenshot image url list
+    app_screenshot = [];
+    for app_url in meta_app_screenshot:
+        url = app_url.get("src");
+        app_screenshot.append(url);
+    app_screenshot = str(app_screenshot);
+    
+    app_data = {
+        'name': app_name,
+        'developer': app_developer,
+        'category': app_category,
+        'rate': app_rate,
+        'score': app_score,
+        'description': app_description,
+        'datepublish': app_datepublish,
+        'softwareVersion': app_softversion,
+        'fileSize': app_filesize,
+        'operatingSystems': app_operatingSystems,
+        'numDownloads': app_downloads,
+        'app_icon_url': app_icon_url,
+        'app_screenshot': app_screenshot,
+        'Google_Play_Link': package_link,
+        'packagename': packagename,
+        }
+    return app_data;
 
-conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='123456',db=database,charset='utf8');
+conn = MySQLdb.connect(host='127.0.0.1',user=username,passwd=password,db=database,charset='utf8');
 cursor = conn.cursor();
 
 def mysql_insert(packagename):
     myDict = get_package_info(packagename);
+    for key in myDict:
+        if myDict[key] is not None:
+            if isinstance(myDict[key],str):
+                myDict[key] = myDict[key].decode('utf8');
+            elif isinstance(myDict[key],int):
+                myDict[key] = str(myDict[key]).decode('utf8');
+            else:
+                myDict[key] = u'' + myDict[key];
+    
     placeholders = ','.join(['%s']* len(myDict));
     colums = ','.join(myDict.keys());
     insert_sql = "INSERT INTO app_info ( %s ) VALUES ( %s )" % (colums,placeholders);
@@ -122,7 +122,6 @@ def mysql_insert(packagename):
 
 def mysql_select(packagename):
     select_sql = "SELECT * FROM app_info WHERE packagename = '%s'" %(packagename);
-    print select_sql;
     flag = cursor.execute(select_sql);
     results = cursor.fetchall();
     return flag;
@@ -146,20 +145,20 @@ def mysql_get_name(category):
         meta_list.append(row[0]);
     return meta_list;
 
-
-category_list = ['BOOKS_AND_REFERENCE','BUSINESS','COMICS','COMMUNICATION','EDUCATION','ENTERTAINMENT','FINANCE','HEALTH_AND_FITNESS','LIBRARIES_AND_DEMO','LIFESTYLE','APP_WALLPAPER','MEDIA_AND_VIDEO','MEDICAL','MUSIC_AND_AUDIO','NEWS_AND_MAGAZINES','PERSONALIZATION','PHOTOGRAPHY','PRODUCTIVITY','SHOPPING','SOCIAL','SPORTS','TOOLS','TRANSPORTATION','TRAVEL_AND_LOCAL','WEATHER'];
-package_list = [];
-
-for category in category_list:
-    meta_list = mysql_get_name(category);
-    package_list.extend(meta_list);
-
-print len(package_list);
-
 if __name__ == '__main__':
+    category_list = ['BOOKS_AND_REFERENCE','BUSINESS','COMICS','COMMUNICATION','EDUCATION','ENTERTAINMENT','FINANCE','HEALTH_AND_FITNESS','LIBRARIES_AND_DEMO','LIFESTYLE','APP_WALLPAPER','MEDIA_AND_VIDEO','MEDICAL','MUSIC_AND_AUDIO','NEWS_AND_MAGAZINES','PERSONALIZATION','PHOTOGRAPHY','PRODUCTIVITY','SHOPPING','SOCIAL','SPORTS','TOOLS','TRANSPORTATION','TRAVEL_AND_LOCAL','WEATHER'];
+    game_subcategory_list = ['GAME_ACTION','GAME_ADVENTURE','GAME_ARCADE','GAME_BOARD','GAME_CARD','GAME_CASINO','GAME_CASUAL','GAME_EDUCATIONAL','GAME_MUSIC','GAME_PUZZLE','GAME_RACING','GAME_ROLE_PLAYING','GAME_SIMULATION','GAME_SPORTS','GAME_STRATEGY','GAME_TRIVIA','GAME_WORD'];
+    category_list.extend(game_subcategory_list);
+    package_list = [];
+    
+    for category in category_list:
+        meta_list = mysql_get_name(category);
+        package_list.extend(meta_list);
+
+    print len(package_list);
+
     for packagename in package_list:
         if mysql_select(packagename):
             continue;
-            #mysql_update(packagename);
         else:
             mysql_insert(packagename);
--- get-Google-Play-package-name.py
+++ get-Google-Play-package-name.py
@@ -1,66 +1,87 @@
+#coding: utf8
 from bs4 import BeautifulSoup
 import urllib2
 import urllib
 import MySQLdb
 import datetime
 
+
 today = str(datetime.date.today());
 database = today + '_google_app';
 database = database.replace('-','_');
-con = MySQLdb.connect(host='127.0.0.1',user='root',passwd='123456');
+con = MySQLdb.connect(host='127.0.0.1',user=username,passwd=password);
 cur = con.cursor();
 create_database_sql = 'CREATE DATABASE ' + database + '   DEFAULT CHARACTER SET utf8   DEFAULT COLLATE utf8_general_ci;';
-print create_database_sql;
 cur.execute(create_database_sql);
-
-conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='123456',db=database,charset='utf8');
+conn = MySQLdb.connect(host='127.0.0.1',user=username,passwd=password,db=database,charset='utf8');
 cursor = conn.cursor();
-
-category_list = ['BOOKS_AND_REFERENCE','BUSINESS','COMICS','COMMUNICATION','EDUCATION','ENTERTAINMENT','FINANCE','HEALTH_AND_FITNESS','LIBRARIES_AND_DEMO','LIFESTYLE','APP_WALLPAPER','MEDIA_AND_VIDEO','MEDICAL','MUSIC_AND_AUDIO','NEWS_AND_MAGAZINES','PERSONALIZATION','PHOTOGRAPHY','PRODUCTIVITY','SHOPPING','SOCIAL','SPORTS','TOOLS','TRANSPORTATION','TRAVEL_AND_LOCAL','WEATHER'];
 
 def create_all_table():
     for category in category_list:
-        sql = 'create table ' + category + ' ( id int primary key auto_increment, packagename varchar(500), paid_rank int(80), free_rank int(80) );';
+        sql = 'create table ' + category + ' ( id int primary key auto_increment, packagename varchar(2000), paid_rank int(80), free_rank int(80), price varchar(20) );';
         cursor.execute(sql);
         conn.commit();
+
+def create_app_info_table():
+    sql = 'create table app_info ( id int primary key auto_increment, packagename varchar(200), description varchar(15000), datepublish varchar(80), fileSize varchar(80), numDownloads varchar(80), category varchar(80), name varchar(80), softwareVersion varchar(80), score varchar(8), operatingSystems varchar(80), developer varchar(100), rate varchar(80), app_icon_url varchar(500), app_screenshot varchar(5000), Google_Play_Link varchar(200) );';
+    cursor.execute(sql);
+    conn.commit();
 
 def get_package(category,price):
     """ 
        type:free or paid
     """
     package_name_list = [];
+    package_price_list = [];
     for num in range(0,9):
-        form_data = {"start": 60 * num,"num": 60,"numChildren": 0,"cctcss": 'square-cover',"cllayout": 'NORMAL',"ipf": 1,"xhr": 1,"token": 'ope8A7jXMKISufw4HA8rzG5_uDI:1459912367279'};
+        form_data = {"start": 60 * num,"num": 60,"numChildren": 0,"cctcss": 'square-cover',"cllayout": 'NORMAL',"ipf": 1,"xhr": 1,"token": 'you can get this token from browser'};
         cookie_str = 'you cookie str';
         link = 'https://play.google.com/store/apps/category/' + category + '/collection/topselling_' + price + '?authuser=0';
         params = urllib.urlencode(form_data);
         request = urllib2.Request(link,params,headers={'Accept-Language': 'en_US','Cookie': cookie_str});
-        web = urllib2.urlopen(request).read();
+        try:
+            web = urllib2.urlopen(request)
+        except urllib2.HTTPError as e:
+            if e.code != 200:
+                print category + 'is' + str(e.code);
+                package_name_list=[];
+                package_price_list=[];
+                return package_name_list,package_price_list;
+        web = web.read();
         data = BeautifulSoup(web,"html5lib");
         meta_data = data.find_all("span","preview-overlay-container");
+        price_data = data.find_all("span","display-price");
         package_list = [];
         for package in meta_data:
             package_name = package.get('data-docid');
             package_list.append(package_name);
         package_name_list.extend(package_list);
-    return package_name_list;
+        package_price = [];
+        for app_price in price_data:
+            pk_price = app_price.string;
+            package_price.append(pk_price);
+        package_price_list.extend(package_price);
+    return package_name_list,package_price_list;
 
 def mysql_insert(category):
-    package_list_free = get_package(category,'free');
+    package_list_free,free_price = get_package(category,'free');
+    print len(package_list_free);
     for free_rank,packagename in enumerate(package_list_free):
-        free_insert_sql = "INSERT INTO %s ( packagename,free_rank ) VALUES ('%s',%i)" % (category,packagename,free_rank);
+        free_insert_sql = u"INSERT INTO %s ( packagename,free_rank,price ) VALUES ('%s',%i,'%s')" % (category,packagename,free_rank,u'free');
         cursor.execute(free_insert_sql);
         conn.commit();
-    package_list_paid = get_package(category,'paid');
+    package_list_paid,paid_price = get_package(category,'paid');
     print len(package_list_paid);
     for paid_rank,packagename in enumerate(package_list_paid):
-        paid_insert_sql = "INSERT INTO %s ( packagename,paid_rank ) VALUES ('%s',%i)" % (category,packagename,paid_rank);
+        paid_insert_sql = u"INSERT INTO %s ( packagename,paid_rank,price ) VALUES ('%s',%i,'%s')" % (category,packagename,paid_rank,paid_price[paid_rank*2]);
         cursor.execute(paid_insert_sql);
         conn.commit();
 
-
-create_all_table();
-
-for category in category_list:
-    print category;
-    mysql_insert(category);
+if __name__ == '__main__':   
+    category_list = ['BOOKS_AND_REFERENCE','BUSINESS','COMICS','COMMUNICATION','EDUCATION','ENTERTAINMENT','FINANCE','HEALTH_AND_FITNESS','LIBRARIES_AND_DEMO','LIFESTYLE','APP_WALLPAPER','MEDIA_AND_VIDEO','MEDICAL','MUSIC_AND_AUDIO','NEWS_AND_MAGAZINES','PERSONALIZATION','PHOTOGRAPHY','PRODUCTIVITY','SHOPPING','SOCIAL','SPORTS','TOOLS','TRANSPORTATION','TRAVEL_AND_LOCAL','WEATHER'];
+    game_subcategory_list = ['GAME_ACTION','GAME_ADVENTURE','GAME_ARCADE','GAME_BOARD','GAME_CARD','GAME_CASINO','GAME_CASUAL','GAME_EDUCATIONAL','GAME_MUSIC','GAME_PUZZLE','GAME_RACING','GAME_ROLE_PLAYING','GAME_SIMULATION','GAME_SPORTS','GAME_STRATEGY','GAME_TRIVIA','GAME_WORD'];
+    category_list.extend(game_subcategory_list);
+    create_all_table();
+    for category in category_list:
+        print category;
+        mysql_insert(category);
